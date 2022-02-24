8629d1583be827546acc7a4545f81bac225ba1cb
jasontan5126 <59143666+jasontan5126@users.noreply.github.com>
2022-02-23 22:52:16 -0500

    Update read_modify_write_sftw_sw0.c

--- lab 2/Task 2 Source Code/read_modify_write_sftw_sw0.c
+++ lab 2/Task 2 Source Code/read_modify_write_sftw_sw0.c
@@ -34,16 +34,29 @@ int main(void)
 	uint8_t n = 3;  //Field value starting from 3
 	uint8_t field_mask = 0x0F; //Field mask 
 	uint8_t field_val; //Read PA3-PA2 and PC1-PC0. 
-	VPORTD_OUT = ~((VPORTA_IN & 0xFC) | (VPORTC_IN & 0x03));
+	
 		
     while (1) 
     {	
 		//Check for if SW0 is press meaning that sends a logic 0
-		field_val = ((VPORTC_IN & 0x03 ) | (VPORTA_IN & 0x0C));
+		VPORTD_OUT = ~((VPORTA_IN & 0xFC) | (VPORTC_IN & 0x03));
+
 		if(!(VPORTB_IN & PIN2_bm)){
 			//							 0b1000 0111           0b1111 PA3 PA2 PC1 PC0 -> 1 PA3 PA2 PC1 PC0 000
-			VPORTD_OUT = (VPORTD_OUT & ~(field_mask << n)) | (((field_val & field_mask) << n) ^ 0x78);
+			while(1){
+				if(!(VPORTB_IN & PIN2_bm)){
+					field_val = ((VPORTC_IN & 0x03 ) | (VPORTA_IN & 0x0C));
+					VPORTD_OUT = (VPORTD_OUT & ~(field_mask << n)) | (((field_val & field_mask) << n) ^ 0x78);	
+				}
+			}
+			
 		}
+		
+		//field_val = ((VPORTC_IN & 0x03 ) | (VPORTA_IN & 0x0C));
+		//if(!(VPORTB_IN & PIN2_bm)){
+			//							 0b1000 0111           0b1111 PA3 PA2 PC1 PC0 -> 1 PA3 PA2 PC1 PC0 000
+		//	VPORTD_OUT = (VPORTD_OUT & ~(field_mask << n)) | (((field_val & field_mask) << n) ^ 0x78);
+		//}
 		
     }
 }
